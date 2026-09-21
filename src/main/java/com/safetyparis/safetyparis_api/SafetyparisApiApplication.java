7209b4f8e4d02023eaf060db6f465783b09a03d0
kimeunseokk <rpqhfls2whdk@gmail.com>
2026-09-21 15:43:24 +0900

    safetyparis-api 커밋 테스트

---
+++ src/main/java/com/safetyparis/safetyparis_api/SafetyparisApiApplication.java
@@ -0,0 +1,13 @@
+package com.safetyparis.safetyparis_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafetyparisApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafetyparisApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/safetyparis/safetyparis_api/SafetyparisApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safetyparis.safetyparis_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafetyparisApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
